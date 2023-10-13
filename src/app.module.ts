c6a8707a2e5724844eddc3946266d492921857db
Niewinski <niewinski.woj@gmail.com>
2023-10-13 09:13:06 +0200

    Installation and configuration of typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config.dbHost,
+      port: config.dbPort,
+      username: config.dbUser,
+      password: config.dbPassword,
+      database: config.dbDatabase,
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      bigNumberStrings: false,
+      logging: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
