77ca7ea77368bf14d13753793b34c8aef4c67e51
Niewinski <niewinski.woj@gmail.com>
2023-10-30 15:08:01 +0100

    CRUD maked to user

---
+++ src/user/dto/add-user.dto.ts
@@ -0,0 +1,5 @@
+export class AddUserDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import {Body, Controller, Delete, Get, Inject, Param, Patch, Post} from '@nestjs/common';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { AddUserDto } from './dto/add-user.dto';
+import { AddUserResponse } from '../types';
+import {UpdateEquipmentDto} from "../equipment/dto/update-equipment.dto";
+import {UpdateUserDto} from "./dto/update-user.dto";
 
 @Controller('user')
 export class UserController {
@@ -9,5 +13,21 @@ export class UserController {
   @Get('/')
   getAllUsers(): Promise<User[]> {
     return this.userService.getAllUsers();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+  @Post('/')
+  addUser(@Body() newUser: AddUserDto): Promise<AddUserResponse> {
+    return this.userService.addUser(newUser);
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.removeUser(id);
   }
 }
